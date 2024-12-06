6ded84a3c8f38ad36549d4e59c515d4f26129832
Brent Reynolds <brentsreynolds5@gmail.com>
2024-12-05 21:03:23 -0500

    Added Dtos

---
+++ api/api/Dtos/ContainerAtPortDto.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Dtos
+{
+    public class ContainerAtPortDto
+    {
+        public string BicCode { get; set; }
+        public decimal SizeTeu { get; set; }
+        public bool IsEmpty { get; set; }
+        public DateTime HandlingDate { get; set; }
+    }
+}
---
+++ api/api/Dtos/ShipAtPortDto.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Dtos
+{
+    public class ShipAtPortDto
+    {
+        public string ShipName { get; set; }
+        public string ImoNumber { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
